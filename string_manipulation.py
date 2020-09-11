8107e0cef1767a87886b3a8772360cb8089ce55b
margaretlclark3 <58199326+margaretlclark3@users.noreply.github.com>
2020-09-11 16:25:02 -0400

    Update string_manipulation.py
    simple string manipulation for lowercase, uppercase, and title with f-string print

--- string_manipulation.py
+++ string_manipulation.py
@@ -7,3 +7,5 @@ lowercase_name = name.lower()
 uppercase_genre = genre.upper()
 
 title_song = song.title()
+
+print(f"My favorite genre is {uppercase_genre} and my favorite song is {title_song} by {lowercase_name}.")
