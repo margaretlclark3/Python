e731a14cc9807009a295da4a7bcdbf5dcfbfb803
margaretlclark3 <58199326+margaretlclark3@users.noreply.github.com>
2020-09-11 16:38:03 -0400

    Simple Create List then Update
    Create list of colors, return two colors, append the color yellow and remove the color tan.

---
+++ create_update_lists.py
@@ -0,0 +1,13 @@
+favorite_colors = ["blue", "pink", "black", "red", "tan", "white"]
+
+# return color blue then red
+
+favorite_colors[0]
+
+favorite_colors[3]
+
+# add color yellow and remove color tan
+
+favorite_colors.append('yellow')
+
+favorite_colors.remove('tan')
