b3e4be1f47db52e4be0098715fd9b9ddd07f0a31
margaretlclark3 <58199326+margaretlclark3@users.noreply.github.com>
2020-09-17 10:21:24 -0400

    Update assigning_objects.py

--- assigning_objects.py
+++ assigning_objects.py
@@ -2,5 +2,6 @@ my_int = 29
 my_float = 400.68
 my_string = "This is a string."
 
+# This is a string., 400.68 is a float, and 29 is an integer --> desired message
 print(f'{my_string} is a string, {my_float} is a float, and {my_int} is an integer')
 
